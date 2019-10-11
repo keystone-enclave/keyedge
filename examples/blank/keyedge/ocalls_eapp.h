9f1bc29117d3ab4548eef2d8bc5e598f2d64f59a
Nisiyama_Suzune <Nisiyama_Suzune@icloud.com>
2019-10-10 17:06:48 -0700

    Added clean script to clean generated code

--- examples/blank/keyedge/ocalls_eapp.h
+++
@@ -1,7 +0,0 @@
-#include "eapp_utils.h"
-#include "edge_call.h"
-#include <syscall.h>
-#include "ocalls.h"
-#include "ocalls_common.h"
-#include "ocalls_builder.h"
-#include "ocalls_reader.h"
--- examples/blank/keyedge/ocalls_host.h
+++
@@ -1,9 +0,0 @@
-#include <keystone.h>
-#include <edge_call.h>
-#include "ocalls.h"
-#include "ocalls_common.h"
-#include "ocalls_builder.h"
-#include "ocalls_reader.h"
-void register_functions() {
-	return;
-}
